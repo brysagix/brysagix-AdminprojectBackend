bc1aaa08210e2f4e15fb71789618dc1229a168b9
Bryan <brysagix@hotmail.com>
2021-12-16 09:45:16 -0500

    aprueba despliegue7

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import { GraphQLServer } from "graphql-yoga";
 //const { GraphQLServer } =require ('graphql-yoga')
 import resolvers from "./graphql/resolvers/index.js";
-import typeDefs from "./graphql/schema.graphql";
 
 //administrar rutas
 
@@ -9,28 +8,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url || URL);
 const __dirname = dirname(__filename);
 
 //config server en Dev
-/*
 export const server = new GraphQLServer({
   typeDefs: path.join(__dirname, "graphql/schema.graphql"),
   resolvers,
 });
 
-export const server = new GraphQLServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  playground: true,
-});
-
-*/
-
-export const server = new GraphQLServer({
-  typeDefs: path.join(__dirname, "graphql/schema.graphql"),
-  resolvers,
-  introspection: true,
-  playground: true,
-});
