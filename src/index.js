634a812cf189563ca9bf7e19756540bc200e8a69
Bryan <brysagix@hotmail.com>
2021-12-16 08:50:08 -0500

    agregando port automático

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import "./database.js"
 
 const puertoLocal = 4000;
 
+//process.env.PORT es el puerto asignado automaticamente en heroku
 server.start({port:process.env.PORT},({port})=>{
 console.log("conectado a",port);
 })
